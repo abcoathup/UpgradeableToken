a90ccedc2ce1a268a11589e2eeab8d5d15dbc788
Andrew B Coathup <andrew@coathup.net>
2019-11-13 16:16:41 +1100

    Fix initialize

--- contracts/Simple721Token.sol
+++ contracts/Simple721Token.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/upgrades/contracts/Initializable.sol";
 
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721Metadata.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721Mintable.sol";
 
-contract Simple721Token is Initializable, ERC721Metadata, ERC721Full, ERC721Mintable {
+contract Simple721Token is Initializable, ERC721Full, ERC721Mintable {
 
     function initialize(address sender) public initializer {
+        ERC721.initialize();
         ERC721Metadata.initialize("Simple721Token", "721");
         ERC721Mintable.initialize(sender);
     }
